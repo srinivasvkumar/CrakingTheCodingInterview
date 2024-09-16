e073e00a406c35f0aef38720d9b637ebc1fd5e5e
srinivasvkumar <srinivasvkumar@gmail.com>
2024-09-16 22:56:05 +0530

    autopep8 formatter on save

--- 1.1UniqueCharecters.py
+++ 1.1UniqueCharecters.py
@@ -2,33 +2,33 @@ import unittest
 
 
 def uniqueIdentifier(str):
-    char_set=[False for _ in range(128)]
-    
+    char_set = [False for _ in range(128)]
+
     for chr in str:
-        val=ord(chr)
+        val = ord(chr)
         if char_set[val]:
             return False
-        else :
-            char_set[val]=True
+        else:
+            char_set[val] = True
     return True
 
 
-
 class Test(unittest.TestCase):
-    dataT=[('abcdef'),('abcd'),('cat'),('')]
-    dataF=[('srinivas'),('mama')]
-    
-    str="mama"
-    print ("result is",uniqueIdentifier(str))     
+    dataT = [('abcdef'), ('abcd'), ('cat'), ('')]
+    dataF = [('srinivas'), ('mama')]
+
+    str = "mama"
+    print("result is", uniqueIdentifier(str))
 
     def test_unique(self):
         for test_string in self.dataT:
-            actual=uniqueIdentifier(test_string)
+            actual = uniqueIdentifier(test_string)
             self.assertTrue(actual)
-            
-        for test_string in self.dataF:
-            actual=uniqueIdentifier(test_string)
-            self.assertFalse(actual)    
 
-if __name__=="__main__": 
-    unittest.main()           
+        for test_string in self.dataF:
+            actual = uniqueIdentifier(test_string)
+            self.assertFalse(actual)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- 1.2StringPermutation.py
+++ 1.2StringPermutation.py
@@ -1,19 +1,18 @@
 from collections import Counter
 
 
-def identifyPermutation(first_str,second_str):
-    if len(first_str)!=len(second_str):
+def identifyPermutation(first_str, second_str):
+    if len(first_str) != len(second_str):
         return False
-    first_str= [_ for _ in first_str]
-    chr=0
+    first_str = [_ for _ in first_str]
+    chr = 0
     while chr < len(second_str):
         if first_str[chr] in second_str:
-            chr+=1
+            chr += 1
             continue
         else:
             return False
     return True
-
 
 
 def check_permutation(str1, str2):
@@ -28,28 +27,30 @@ def check_permutation(str1, str2):
         counter[c] -= 1
     return True
 
-def comBySort(str1,str2):
-    j=0
-    counterStr2=sorted(str2)
-    counterStr1=sorted(str1)
+
+def comBySort(str1, str2):
+    j = 0
+    counterStr2 = sorted(str2)
+    counterStr1 = sorted(str1)
    # print("first",counterStr1)
    # print("second",counterStr2)
     for i in str2:
-        if counterStr2[j]==counterStr1[j]:
-            j+=1
+        if counterStr2[j] == counterStr1[j]:
+            j += 1
             continue
         else:
             return False
     return True
-            
-    
 
 
-str1="savinirs" # have given srinivas in reverse
-str2="ssrinvai"
+str1 = "savinirs"  # have given srinivas in reverse
+str2 = "ssrinvai"
 
-print("strings comparison by trail method - many times wrong:-",identifyPermutation(str1,str2)) #yeilds wrong result
+print("strings comparison by trail method - many times wrong:-",
+      identifyPermutation(str1, str2))  # yeilds wrong result
 
-print("strings comparison by counter method:-",check_permutation(str1,str2)) #correct algo
+print("strings comparison by counter method:-",
+      check_permutation(str1, str2))  # correct algo
 
-print("strings comparison by sort method:-",comBySort(str1,str2)) #compare by sorting 
+print("strings comparison by sort method:-",
+      comBySort(str1, str2))  # compare by sorting
