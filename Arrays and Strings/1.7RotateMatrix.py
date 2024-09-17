464f1fa7ad6a876338508e0f1b61c0f5209a652e
srinivasvkumar <srinivasvkumar@gmail.com>
2024-09-17 21:52:50 +0530

    full byheart. there is no way to understand and solve this.

---
+++ Arrays and Strings/1.7RotateMatrix.py
@@ -0,0 +1,48 @@
+def rotate_matrix(matrix):
+    '''rotates a matrix 90 degrees clockwise'''
+    n = len(matrix)
+    for layer in range(n // 2):
+        first, last = layer, n - layer - 1
+        for i in range(first, last):
+            # save top
+            top = matrix[layer][i]
+
+            # left -> top
+            matrix[layer][i] = matrix[-i - 1][layer]
+
+            # bottom -> left
+            matrix[-i - 1][layer] = matrix[-layer - 1][-i - 1]
+
+            # right -> bottom
+            matrix[-layer - 1][-i - 1] = matrix[i][- layer - 1]
+
+            # top -> right
+            matrix[i][- layer - 1] = top
+    return matrix
+
+# data = [
+#         ([
+#             [1, 2, 3, 4, 5],
+#             [6, 7, 8, 9, 10],
+#             [11, 12, 13, 14, 15],
+#             [16, 17, 18, 19, 20],
+#             [21, 22, 23, 24, 25]
+#         ], [
+#             [21, 16, 11, 6, 1],
+#             [22, 17, 12, 7, 2],
+#             [23, 18, 13, 8, 3],
+#             [24, 19, 14, 9, 4],
+#             [25, 20, 15, 10, 5]
+#         ])
+#     ]
+
+
+test_matrix = [
+    [1, 2, 3, 4, 5],
+    [6, 7, 8, 9, 10],
+    [11, 12, 13, 14, 15],
+    [16, 17, 18, 19, 20],
+    [21, 22, 23, 24, 25]
+]
+
+print(rotate_matrix(test_matrix))
